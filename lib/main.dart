a51e1aa75e5d2dafdb74cbf24eec8cca77b8e9af
Jack <john@trainsarecool.com>
2020-11-05 23:37:01 +0700

    Toast not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,105 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 import 'NYCWGrid.dart';
 import 'CWKB.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class CustomPopupMenu {
+  CustomPopupMenu({this.title, this.icon});
+
+  String title;
+  IconData icon;
+
+  List choices = [
+    CustomPopupMenu(title: 'Home', icon: Icons.home),
+    CustomPopupMenu(title: 'Bookmarks', icon: Icons.bookmark),
+    CustomPopupMenu(title: 'Settings', icon: Icons.settings),
+  ];
+
+  Widget myPopMenu() {
+    return PopupMenuButton(
+        onSelected: (value) {
+          Fluttertoast.showToast(
+              msg: "You have selected " + value.toString(),
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 1,
+              backgroundColor: Colors.black,
+              textColor: Colors.white,
+              fontSize: 16.0);
+        },
+        itemBuilder: (context) => [
+              PopupMenuItem(
+                  value: 1,
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(2, 2, 8, 2),
+                        child: Icon(Icons.print),
+                      ),
+                      Text('Print')
+                    ],
+                  )),
+              PopupMenuItem(
+                  value: 2,
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(2, 2, 8, 2),
+                        child: Icon(Icons.share),
+                      ),
+                      Text('Share')
+                    ],
+                  )),
+              PopupMenuItem(
+                  value: 3,
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(2, 2, 8, 2),
+                        child: Icon(Icons.add_circle),
+                      ),
+                      Text('Add')
+                    ],
+                  )),
+            ]);
+  }
+
+  Widget myGridView() {
+    return GridView.count(
+      crossAxisCount: 2,
+      padding: EdgeInsets.all(15),
+      crossAxisSpacing: 15,
+      mainAxisSpacing: 5,
+      children: <Widget>[
+        Container(
+            color: Colors.amberAccent,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  child: myPopMenu(),
+                  right: 0,
+                )
+              ],
+            )),
+        Container(
+            color: Colors.deepOrangeAccent,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  child: myPopMenu(),
+                  right: 0,
+                )
+              ],
+            )),
+      ],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -38,7 +133,26 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(widget.title),
+          new RaisedButton(
+            padding: const EdgeInsets.all(8.0),
+            textColor: Colors.black,
+            color: Colors.red,
+            onPressed: () {
+              Fluttertoast.showToast(
+                  msg: "This is Center Short Toast",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 16.0);
+            },
+            child: new Text("Menu"),
+          ),
+        ]),
       ),
       body: Center(
         child: Column(
@@ -47,10 +161,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
+      //   onPressed: () {},
       //   tooltip: 'Increment',
       //   child: Icon(Icons.add),
-      //), // This trailing comma makes auto-formatting nicer for build methods.
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
