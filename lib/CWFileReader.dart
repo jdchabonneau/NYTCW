b25b4e24370ffaff4fd9d47cb51eae736c2a9584
Jack <john@trainsarecool.com>
2020-11-14 22:23:50 +0700

    Parsing input strng

---
+++ lib/CWFileReader.dart
@@ -0,0 +1,10 @@
+import 'package:nytCW/puzzles.dart';
+
+class CWFileReader {
+  Map words = Map();
+  Map definitions = Map();
+
+  static void parseFile(String pathToFile) {
+    var s = D.jjj();
+  }
+}
---
+++ lib/puzzles.dart
@@ -0,0 +1,60 @@
+// risky;Ripe;Like cutting your own hair
+// intro;First paragraph of a five paragraph essay
+// plaid;stage
+// eagle;Play ground?
+// -well;Tiny food for the world's largest animal
+
+// ##1
+// 5X5
+// 1:--jut
+// 4:peach
+// 6:ladle
+// 7:ocean
+// 8:phd--
+
+// 1A:Stick (out)
+// 1D:Bored with the whole thing
+// 2D:Sch. that receives the most college applications each year (111,000)
+// 3D:"What happened next was:.."
+// 4A:Fruit that lends its name to a Georgia county
+// 4D:Raindrop sound
+// 5D:Apiece
+// 6A:Soup scooper
+// 7A:Atlantic or Pacific
+// 8A:Distinguished degree
+class D {
+  static String jjj() {
+    return ''' 
+##2 
+7X7
+1:bff-4:tab
+7:alibaba
+9:surebet
+10:--ego--
+11:drfauci
+15:moonlit
+16:vex-17:ins
+
+1A:Lifelong pal
+1D:Some undergrad degrees
+2D:Reason for a seasonal shot
+3D:Alternative to Chrome or Safari
+4A:Indenting key
+4D:Salad of finely chopped bulgur, parsely, tomatoes and moonlit
+5D:Father on "The Marvelous Mrs. Maisel"
+6D:Dracula's other form
+7A:China's largest e-commerece company
+8D:Started
+9A:Wager that can't miss
+10A:Self-image
+11A:HealthExpert given the 2020 Federal Employee of the Year Award
+11D:Government agcy. with notorious long lines
+12D:FishEggs
+13D:The Bengals on a sports ticker
+14D:'___ all good'
+15A:moonlit
+16A:Irritate
+17A:Shoo___ (overwhelming favorites)
+''';
+  }
+}
